ca47a2f0578e88901dd0c2ead511ee55fe9d29e6
Apurba1903 <apurba1903@gmail.com>
2023-08-10 03:59:31 +0600

    find off sum done

--- findOddSum.js
+++ findOddSum.js
@@ -1,14 +1,32 @@
 function getSumOfAnArray (numbers){
     let sum = 0;
-
     for (let i = 0; i < numbers.length; i++){
         const index = i;
         const elements = numbers[index];
         sum = sum+elements;
-        console.log(index, elements, sum);
+        // console.log(index, elements, sum);
     }
     return sum;
 }
 
-const myNumbers = [20, 12, 11, 55, 60, 20, 10, 69];
+function getOddNumberOfAnArray (numbers){
+    const oddnumber = [];
+    for (let i = 0; i < numbers.length; i++){
+        const index = i;
+        const elements = numbers[index];
+        if (elements % 2 === 1){
+            console.log(index, elements);
+            oddnumber.push(elements);
+        }
+    }
+    return oddnumber;
+}
+const myNumbers = [20, 12, 11, 55, 60, 20, 61, 69];
 getSumOfAnArray(myNumbers);
+
+const oddNumbers = getOddNumberOfAnArray(myNumbers);
+console.log(oddNumbers);
+
+const oddNumberSum = getSumOfAnArray(oddNumbers);
+console.log('The sum of odd number is ', oddNumberSum);
+
